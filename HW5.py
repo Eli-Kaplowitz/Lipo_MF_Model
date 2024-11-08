397a9c15fae1ebf3f0343385d2d3c6e10b1962fa
Eli K <elikaplowitz@gmail.com>
2024-11-08 11:20:33 -0500

    Writing results to txt file

--- HW5.py
+++ HW5.py
@@ -11,7 +11,8 @@ from rdkit.Chem import rdFingerprintGenerator
 import argparse
 
 dataset = "Lipophilicity.csv"
-print(os.getenv("CONDA_DEFAULT_ENV"))
+env_name = os.getenv("CONDA_DEFAULT_ENV")
+print(f'Current Environment: {env_name}')
 
 # Load the dataset
 data = pd.read_csv(dataset)
@@ -53,14 +54,24 @@ max_iter = args.max_iter
 
 print(f'Using hidden_layer_sizes: {hidden_layer_sizes} and max_iter: {max_iter}')
 
-
-#Morgan Fingerprint
-
 def morgan_fingerprint(smiles):
     mol = Chem.MolFromSmiles(smiles)
     generator = rdFingerprintGenerator.GetMorganGenerator(radius=2, fpSize=1024)
     return generator.GetFingerprintAsNumPy(mol)
 
+def maccs_keys(smiles):
+    mol = Chem.MolFromSmiles(smiles)
+    return [int(x) for x in AllChem.GetMACCSKeysFingerprint(mol)]
+
+def calculate_rmse(y_true, y_pred):
+    residuals = y_true - y_pred
+    squared_residuals = residuals ** 2
+    mean_squared_residuals = np.mean(squared_residuals)
+    rmse = np.sqrt(mean_squared_residuals)
+    return rmse
+
+
+#Morgan Fingerprint
 
 train = X_train['smiles'].apply(morgan_fingerprint)
 test = X_test['smiles'].apply(morgan_fingerprint)
@@ -83,12 +94,12 @@ X_train
 #morgan fingerprint model
 model = MLPRegressor(hidden_layer_sizes=(hidden_layer_sizes), max_iter=max_iter, random_state=42)
 model.fit(X_train, y_train.ravel())
-morgan_score = model.score(X_test, y_test)
-print(f'Morgan score: {morgan_score}')
+#morgan_score = model.score(X_test, y_test)
+#print(f'Morgan score: {morgan_score}')
 
 #RMSE unscaled targets
 y_pred = model.predict(X_test)
-morgan_rmse = mean_squared_error(y_test, y_pred, squared=False)
+morgan_rmse = calculate_rmse(y_test, y_pred)
 print(f'Morgan RMSE: {morgan_rmse}')
 
 
@@ -98,9 +109,7 @@ y_test = targets_test_scaled
 X_train = features
 y_train = targets_scaled
 
-def maccs_keys(smiles):
-    mol = Chem.MolFromSmiles(smiles)
-    return [int(x) for x in AllChem.GetMACCSKeysFingerprint(mol)]
+
 
 X_train['MACCS'] = X_train['smiles'].apply(maccs_keys)
 X_test['MACCS'] = X_test['smiles'].apply(maccs_keys)
@@ -117,10 +126,19 @@ X_train.head()
 #MACCS keys model
 model = MLPRegressor(hidden_layer_sizes=(100, 100), max_iter=1000, random_state=42)
 model.fit(X_train, y_train.ravel())
-maccs_score = model.score(X_test, y_test)
-print(f'MACCS score: {maccs_score}')
+#maccs_score = model.score(X_test, y_test)
+#print(f'MACCS score: {maccs_score}')
 
 #RMSE unscaled targets
 y_pred = model.predict(X_test)
-maccs_rmse = mean_squared_error(y_test, y_pred, squared=False)
+maccs_rmse = calculate_rmse(y_test, y_pred)
 print(f'MACCS RMSE: {maccs_rmse}')
+
+# Save the test set RMSE, conda environment name, and hyperparameters to a file
+with open('results.txt', 'w') as f:
+    f.write(f'Environment: {env_name}\n')
+    f.write('Hyperparameters:\n')
+    f.write(f'Hidden Layer Sizes: {hidden_layer_sizes}\n')
+    f.write(f'Max Iterations: {max_iter}\n')
+    f.write(f'Morgan RMSE: {morgan_rmse}\n')
+    f.write(f'MACCS RMSE: {maccs_rmse}\n')
